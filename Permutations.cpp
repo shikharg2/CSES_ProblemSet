bbcc9ec762ef768b7cb5e4ee487436cbbbe71bca
shikharg2 <67795237+shikharg2@users.noreply.github.com>
2024-11-18 20:48:37 +0530

    Add files via upload

---
+++ Permutations.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if(n == 2 || n == 3){
+        cout << "NO SOLUTION";
+        return 0;
+    }
+
+    for(int i = 1 ; i <= n ; i++){
+        if(i % 2 == 0){
+            cout << i << " ";
+        }
+    }
+    for(int i = 1 ; i <= n ; i++){
+        if(i % 2 == 1){
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
